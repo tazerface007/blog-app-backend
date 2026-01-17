dd5e420079fc46f8aefa5adb00e8a71d2ff1bffe
tazerface007 <rohandesoza@duck.com>
2026-01-17 22:24:29 +0530

    Add db folders

---
+++ app/db/models/usermodel.py
@@ -0,0 +1,15 @@
+from db import db
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def json(self):
+        return {'id': self.id, 'username': self.username, 'email': self.email}
