ef4b030d7fccd5753a8b43c1a4175b90a8ee7515
tazerface007 <rohandesoza@duck.com>
2026-01-17 22:24:02 +0530

    Add config and db migration suport

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,23 @@
 from flask import Flask, request, jsonify
 import os
 from dotenv import load_dotenv
+from .db import db
+from config import Config
+from flask_migrate import Migrate
 
 load_dotenv()
 
+migrate = Migrate()
+
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
+    with app.app_context():
+        db.create_all()
     # Load configuration from environment variables
-    
     # Blueprint registration
     from app.routes import api_bp
     app.register_blueprint(api_bp)
