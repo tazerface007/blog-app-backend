5f17c705920a1ec57d73cf74724df591e87025c4
tazerface007 <rohandesoza@duck.com>
2026-01-17 20:58:55 +0530

    Add blueprint registration and create_app method

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,15 @@
-from .app import app
+from flask import Flask, request, jsonify
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Load configuration from environment variables
+    
+    # Blueprint registration
+    from app.routes import api_bp
+    app.register_blueprint(api_bp)
+    return app
