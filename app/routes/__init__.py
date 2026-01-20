5e790fcb5e6a8444d23bdd39cb27d723fa092e45
tazerface007 <rohandesoza@duck.com>
2026-01-20 15:31:26 +0530

    Add 404 handler bp

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,9 +3,11 @@ from flask import Blueprint
 from .homeroute import home_bp
 from .userroute import user_bp
 from .blogroute import blog_bp
+from .analytics import analytics_bp
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
 api_bp.register_blueprint(home_bp)
 api_bp.register_blueprint(user_bp)
 api_bp.register_blueprint(blog_bp)
+api_bp.register_blueprint(analytics_bp)
