e2891fe7cc8e5e1e26840dde836ad6259aed8b2b
tazerface007 <rohandesoza@duck.com>
2026-01-20 14:04:08 +0530

    Add conditionally run debug/prod.

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from waitress import serve
 
 if __name__ == '__main__':
     app = create_app()
-    serve(app, host='0.0.0.0', port=8000)
+    print('Starting the Flask application...')
+    if app.config["DEBUG"] is False:
+        serve(app, host='0.0.0.0', port=8000)
+    else:
+        app.run(debug=True, port=8000)
