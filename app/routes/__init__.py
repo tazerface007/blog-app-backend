f6105fa72ea8d28ace5d7a6086bb30b16ab131fb
tazerface007 <rohandesoza@duck.com>
2026-01-24 17:26:50 +0530

    Add userroute

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,6 +4,7 @@ from .homeroute import home_bp
 from .userroute import user_bp
 from .blogroute import blog_bp
 from .analytics import analytics_bp
+from .login import login_bp
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -11,3 +12,4 @@ api_bp.register_blueprint(home_bp)
 api_bp.register_blueprint(user_bp)
 api_bp.register_blueprint(blog_bp)
 api_bp.register_blueprint(analytics_bp)
+api_bp.register_blueprint(login_bp)
