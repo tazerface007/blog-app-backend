6e2531ddbbe89689beb66f1a84ddbb3b726034b8
tazerface007 <rohandesoza@duck.com>
2026-01-26 23:09:48 +0530

    update DB url on  config.py

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(BASE_DIR, 'app.db'))
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(BASE_DIR,'instance', 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = os.getenv("FLASK_DEBUG", "0").lower() in ("1", "true", "yes", "True")
+    def __init__(self):
+        print(Config.SQLALCHEMY_DATABASE_URI)
