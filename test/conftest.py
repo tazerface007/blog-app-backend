acd036e2c0e57615be1895d78fccdf0593cfc0c4
tazerface007 <rohandesoza@duck.com>
2026-01-28 13:49:52 +0530

    update

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,21 @@
 import pytest
 from app import create_app
+from app.db import db
+
+@pytest.fixture
+def app():
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+        yield app
 
 @pytest.fixture
 def client():
     app = create_app()
-    app.config['TESTING'] = True
+    app.config.update({
+        "TESTING": True,
+    })
+
     with app.test_client() as client:
         yield client
+
--- test/test_project.py
+++ test/test_project.py
@@ -1,3 +1,25 @@
+import pytest
+
+
 def test_home_route(client):
-    response = client.get('/')
+    response = client.get('/api/')
     assert response.status_code == 200
+    data = response.get_json()
+    assert data['message'] == 'Welcome to the Home Route!'
+
+
+def test_blog_route(client):
+    response = client.get('/api/blog/')
+    # print(response.status_code)
+    # assert response.status_code == 200
+    data = response.get_json()
+    # assert data['message'] == 'Welcome to the Home Route!'
+
+def test_get_all_metadata(app):
+    from app.services.metadata_db_services import create_metadata_entry, get_all_metadata_entry
+    with app.app_context():
+        create_metadata_entry('Blog App','My Awesome Blog App','/blog/app','github.com/path')
+        list = get_all_metadata_entry()
+        print('list:', list)
+        
+    
