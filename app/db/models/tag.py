4271c77a23ff8bc8210db160db81000043c5a273
tazerface007 <rohandesoza@duck.com>
2026-02-06 14:48:36 +0530

    Update & Remove foreign keys

--- app/db/models/tag.py
+++ app/db/models/tag.py
@@ -1,11 +1,11 @@
 from app.db import db
 
 
-post_tags = db.Table(
-    'post_tags',
-    db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True)
-)
+# post_tags = db.Table(
+#     'post_tags',
+#     db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True),
+#     db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+# )
 
 class Tag(db.Model):
     __tablename__ = 'tags'
