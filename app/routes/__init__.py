32992488b8f08b0f0b92a07517b371818aa7f780
tazerface007 <rohandesoza@duck.com>
2026-02-06 12:00:23 +0530

    Add blog_admin_bp register

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 from .homeroute import home_bp
 from .userroute import user_bp
-from .blogroute import blog_bp
+from .blogroute import blog_bp,blog_admin_bp
 from .analytics import analytics_bp
 from .login import login_bp
 from .projectroute import project_bp
@@ -12,6 +12,7 @@ api_bp = Blueprint('api', __name__, url_prefix='/api')
 api_bp.register_blueprint(home_bp)
 api_bp.register_blueprint(user_bp)
 api_bp.register_blueprint(blog_bp)
+api_bp.register_blueprint(blog_admin_bp)
 api_bp.register_blueprint(analytics_bp)
 api_bp.register_blueprint(login_bp)
 api_bp.register_blueprint(project_bp)
